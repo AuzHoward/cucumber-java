96c9173053c06bbf68a2f06c098917032b0c809d
Auz <austinleehoward@gmail.com>
2021-04-25 01:54:40 -0500

    First commit

--- src/main/java/io/cucumber/skeleton/Belly.java
+++ src/main/java/io/cucumber/skeleton/Belly.java
@@ -2,6 +2,8 @@ package io.cucumber.skeleton;
 
 public class Belly {
     public void eat(int cukes) {
-
+        if(cukes > 0){
+            System.out.println("YUMMMMM YUMMMMM YUMMM");
+        }else{System.out.println("Wheres the Cukes");}
     }
 }
--- src/test/java/io/cucumber/skeleton/StepDefinitions.java
+++ src/test/java/io/cucumber/skeleton/StepDefinitions.java
@@ -1,11 +1,27 @@
 package io.cucumber.skeleton;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class StepDefinitions {
     @Given("I have {int} cukes in my belly")
     public void I_have_cukes_in_my_belly(int cukes) {
         Belly belly = new Belly();
         belly.eat(cukes);
+    }
+
+    @When("I wait {int} hour")
+    public boolean I_wait_an_hour(int hour){
+        if(hour == 1){
+            return true;
+        }else{return false;}
+    }
+
+    @Then("my belly should growl")
+    public void belly_growl(){
+        if(I_wait_an_hour(1) == true){
+            System.out.println("GRRRRRRRRRRRRRRRRRR");
+        }else{System.out.println("PLEASE WAIT AN HOUR");}
     }
 }
