a8ac422c17a358d4073f08749827020f849afe14
Auz <austinleehoward@gmail.com>
2021-05-04 09:32:06 -0500

    protected Variables

--- src/test/java/io/cucumber/skeleton/Element.java
+++ src/test/java/io/cucumber/skeleton/Element.java
@@ -1,16 +1,16 @@
 package io.cucumber.skeleton;
 
-public class Element {
-    public static String googleURL = "https://google.com";
-    public static String southwestURL = "https://southwest.com";
+public abstract class Element {
+    protected String googleURL = "https://google.com";
+    protected String southwestURL = "https://southwest.com";
 
     //Round Trip and One-way Radio buttons
-    public static String roundTripRadio = "#TabbedArea_4-panel-0 > div > div > div > form > div.search-form--options > div.search-form--options-trip > fieldset > ul > li:nth-child(1) > label > input";
-    public static String oneWayRadio = "#TabbedArea_4-panel-0 > div > div > div > form > div.search-form--options > div.search-form--options-trip > fieldset > ul > li:nth-child(2) > label > input";
+    protected String roundTripRadio = "#TabbedArea_4-panel-0 > div > div > div > form > div.search-form--options > div.search-form--options-trip > fieldset > ul > li:nth-child(1) > label > input";
+    protected String oneWayRadio = "#TabbedArea_4-panel-0 > div > div > div > form > div.search-form--options > div.search-form--options-trip > fieldset > ul > li:nth-child(2) > label > input";
 
     //From and To Airport Fields
-    public static String fromAirportField = "#LandingAirBookingSearchForm_originationAirportCode";
-    public static String toAirportField = "#LandingAirBookingSearchForm_destinationAirportCode";
+    protected String fromAirportField = "#LandingAirBookingSearchForm_originationAirportCode";
+    protected String toAirportField = "#LandingAirBookingSearchForm_destinationAirportCode";
 
-    public static String searchFlightButton = "#LandingAirBookingSearchForm_submit-button";
+    protected String searchFlightButton = "#LandingAirBookingSearchForm_submit-button";
 }
